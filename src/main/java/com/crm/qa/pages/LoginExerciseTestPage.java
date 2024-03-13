9cdbbfe52e258c634ba70ba9d76da54a647cd197
Luis Fernando Martinez Galvez <luferma06@yahoo.es>
2024-03-12 21:21:50 -0500

    Uploaded page object project

---
+++ src/main/java/com/crm/qa/pages/LoginExerciseTestPage.java
@@ -0,0 +1,65 @@
+package com.crm.qa.pages;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.util.TestUtil;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+import java.util.List;
+
+public class LoginExerciseTestPage extends TestBase{
+
+	//Page Factory - OR:
+	@FindBy(xpath="//span[contains(text(),'Search')]")
+	WebElement searchText;
+
+	@FindBy(xpath="//span[contains(@class,'boxzilla-close-icon')]")
+	WebElement closePopUp;
+
+	@FindBy(xpath="//input[contains(@type,'search')]")
+	WebElement searchEnterText;
+
+	@FindBy(xpath="//article[contains(@class,'display-flex')]//div//h3")
+	List<WebElement> resultSearchText;
+
+	TestUtil testUtil;
+
+	//Initializing the Page Objects:
+	public LoginExerciseTestPage(){
+		PageFactory.initElements(driver, this);
+		testUtil = new TestUtil();
+	}
+
+	public LoginExerciseTestPage searchText(){
+		testUtil.waitVisibility(closePopUp);
+		closePopUp.click();
+
+		testUtil.waitVisibility(searchText);
+		searchText.click();
+
+		searchEnterText.click();
+		searchEnterText.sendKeys("Test");
+		searchEnterText.sendKeys(Keys.ENTER);
+
+		boolean flag = false;
+		for(WebElement element: resultSearchText){
+			System.out.println("Text: " + element.getText());
+			if(element.getText().contains("TEST-DRIVEN")){
+				flag=true;
+				break;
+			}
+		}
+
+		Assert.assertTrue(flag);
+
+		return new LoginExerciseTestPage();
+	}
+
+	public void buyElements(){
+
+	}
+}
---
+++ src/test/java/com/crm/qa/testcases/HomePageExerciseTest.java
@@ -0,0 +1,38 @@
+package com.crm.qa.testcases;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.LoginExercisePage;
+import com.crm.qa.pages.LoginExerciseTestPage;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HomePageExerciseTest extends TestBase {
+	LoginExerciseTestPage loginExerciseTestPage;
+
+	public HomePageExerciseTest() {
+		super();
+	}
+
+	//test cases should be separated -- independent with each other
+	//before each test case -- launch the browser and login
+	//@test -- execute test case
+	//after each test case -- close the browser
+	
+	@BeforeMethod
+	public void setUp() {
+		initialization();
+	}
+
+	@Test(priority=1)
+	public void loginTest(){
+		loginExerciseTestPage = new LoginExerciseTestPage();
+		loginExerciseTestPage.searchText();
+	}
+
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+
+}
